f8d732714e6f6d74039f7b7c630738ca1a62b0f6
Jason Lewis <pensfan5050@gmail.com>
2023-04-02 16:52:03 -0400

    added coded to fetch weather from API. 5th commit

--- script.js
+++ script.js
@@ -10,3 +10,49 @@ var cityEl = $('#city');
 var fiveForecastEl = $('#five-forecast');
 var weatherIcon = 'http://openweathermap.org/img/wn/';
 var citySearch = [];
+// Fetch weather data from API url
+
+function fetchWeather(city) {
+    var coordinates = `${openWeatherUrl}${city}&appid=${ApiKey}`
+    fetch (coordinates) 
+        .then (function (coordinateResult) {
+            if (coordinateResult.ok) {
+                coordinateResult.json().then(function (data) {
+                    var lon = data.coord.lon;
+                    var lat = data.coord.lat;
+                    var apiFetch = forecastUrl + lat + '&lon=' + lon + '&appid=' + ApiKey + '&units=metric';
+
+                    fetch(apiFetch)
+                        .then(function (weatherResult) {
+                            if (weatherResult.ok) {
+                                weatherResult.json().then(function (weatherInfo) {
+
+                                    // current day container
+                                    
+                                    var weatherNow = $('<div></div>')
+                                    .attr({ id: 'weather-now'})
+                                    var current = weatherInfo.list[0];
+                                    var icon = current.weather[0].icon;
+                                    var cityIcon = weatherIcon + icon + '.png';
+                                    var iconEL = $('<img>')
+                                        .attr({ id: 'weather-current-icon',
+                                                src: cityIcon,
+                                                alt: 'Icon for weather'});
+
+                                    var currentHeading = $('<h2></h2>')
+                                        .text(city + ' (' + currentDate + ')');
+
+                                    var weatherListCurrent = $('<ul></ul>');
+
+                                    var weatherDetailsCurrent = ['Temp: ' + current.main.temp + ' °C', 'Wind: ' + current.wind.speed + ' MPH', 'Humidity: ' + current.main.humidity + '%']
+                                    
+                                    for (var i = 0; i < weatherDetailsCurrent.length; i++) {
+                                        var currentWeatherItem = $('<li></li>')
+                                            .text(weatherDetailsCurrent[i])
+                                        weatherListCurrent.append(currentWeatherItem);    
+                                    }
+
+                                    weatherNow.append(currentHeading);
+                                    currentHeading.append(iconEL);
+                                    weatherNow.append(weatherListCurrent);
+                                    $('#five-forecast').before(weatherNow);
